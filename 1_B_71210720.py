2e60b0a2dc2f0da874a4b9ca02733fa98051c6a2
justineww <93313520+justineww@users.noreply.github.com>
2021-11-19 02:51:49 +0700

    Add files via upload

---
+++ 1_B_71210720.py
@@ -0,0 +1,21 @@
+n=str(input("Masukkan nilai huruf PrakTekkom anda:"))
+if n =="E":
+    print("Rentang nilai PrakTekKom anda adalah 0 - 44")
+elif n=="D":
+    print("Rentang nilai PrakTekKom anda adalah 45 - 54")
+elif n=="C": 
+    print("Rentang nilai PrakTekKom anda adalah 55 - 59")
+elif n=="C+":
+    print("Rentang nilai PrakTekKom anda adalah 60 - 64")
+elif n=="B-":
+    print("Rentang nilai PrakTekKom anda adalah 65 - 69")
+elif n=="B":
+    print("Rentang nilai PrakTekKom anda adalah 70 - 74")
+elif n=="B+":
+    print("Rentang nilai PrakTekKom anda adalah 75 - 79")
+elif n=="A-":
+    print("Rentang nilai PrakTekKom anda adalah 80 - 84")
+elif n=="A":
+    print("Rentang nilai PrakTekKom anda adalah 85 - 100")
+else:
+    print("Inputan anda salah atau nilai huruf tidak ada pada Standar Nilai")
